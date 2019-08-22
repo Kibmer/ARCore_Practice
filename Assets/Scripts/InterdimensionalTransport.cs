c7f63934fd2701b1e440096a2151d47887f13bec
Kibmer <kkb9243@naver.com>
2019-08-22 10:49:06 +0900

    Improve rendering system
    - 카메라와 카메라 시야 절두체 사이에  stencil 필터가 들어갔을 때, 깜빡이는 문제 stencil 필터가 들어간 오브젝트의 렌더러를 껐다 키는 것으로 해결.
    - 안과 밖을 비교하는 것을 z 좌표로 하는 것이 아니라 포탈과 카메라간의 축을 만들어 그 값을 비교해야함

--- Assets/Scripts/InterdimensionalTransport.cs
+++ Assets/Scripts/InterdimensionalTransport.cs
@@ -5,51 +5,58 @@ using UnityEngine.Rendering;
 
 public class InterdimensionalTransport : MonoBehaviour
 {
-   public Material[] materials;
+    public Material[] materials;
+    public MeshRenderer portalWindow;
 
-   void Start()
-   {
-       foreach (var mat in materials)
-       {
-           mat.SetInt("_StencilTest", (int)CompareFunction.Equal);
-       }
-   }
+    void Start()
+    {
+        foreach (var mat in materials)
+        {
+            mat.SetInt("_StencilTest", (int)CompareFunction.Equal);
+        }
+    }
 
-   private void OnTriggerStay(Collider other)
-   {
-       if (other.name != "ARCoreCamera") return;
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.name != "ARCoreCamera") return;
 
+        if (transform.position.z >= other.transform.position.z - 0.01f)
+        {
+            // Outside of other world
+            Debug.Log("Outside of other world");
+            if (portalWindow.enabled == false)
+            {
+                portalWindow.enabled = true;
+            }
+            foreach (var mat in materials)
+            {
+                mat.SetInt("_StencilTest", (int)CompareFunction.Equal);
+            }
+        }
+        else
+        {
+            // Inside other dimension
+            Debug.Log("Inside Other World");
+            if (portalWindow.enabled == true)
+            {
+                portalWindow.enabled = false;
+            }
+            foreach (var mat in materials)
+            {
+                mat.SetInt("_StencilTest", (int)CompareFunction.NotEqual);
+            }
+        }
+    }
+    private void OnDestroy()
+    {
+        foreach (var mat in materials)
+        {
+            mat.SetInt("_StencilTest", (int)CompareFunction.NotEqual);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
 
-
-       if (transform.position.z > other.transform.position.z)
-       {
-           // Outside of other world
-           Debug.Log("Outside of other world");
-           foreach (var mat in materials)
-           {
-               mat.SetInt("_StencilTest", (int)CompareFunction.Equal);
-           }
-       }
-       else
-       {
-           // Inside other dimension
-           Debug.Log("Inside Other World");
-           foreach (var mat in materials)
-           {
-               mat.SetInt("_StencilTest", (int)CompareFunction.NotEqual);
-           }
-       }
-   }
-   private void OnDestroy()
-   {
-       foreach (var mat in materials)
-       {
-           mat.SetInt("_StencilTest", (int)CompareFunction.NotEqual);
-       }
-   }
-   // Update is called once per frame
-   void Update()
-   {
-
-   }
+    }
 }
